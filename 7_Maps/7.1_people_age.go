222220c13d93e1cd2660d06ad989702b6a08959c
rcgc <roberto140298@gmail.com>
2024-10-16 12:42:40 -0600

    program 7.1 maps modified

--- 7_Maps/7.1_people_age.go
+++ 7_Maps/7.1_people_age.go
@@ -4,20 +4,15 @@ import "fmt"
 
 func main() {
 	limit := 10
-
 	var m1 = map[string]int{"Juan": 33, "John": 40, "Mary": 21, "Reyna": 29}
-
-	m2 := map[string]string{"Mexico": "MX", "United States": "US", "Germany": "GER", "New Zealand": "NZL"}
-
+	m2 := map[string]string{"MX":"Mexico", "US":"United States", "GER":"Germany", "NZL":"New Zealand"}
 	m3 := make(map[string]int)
-
 	fmt.Printf("m1: (%T) %v\n", m1, m1)
 
 	fmt.Printf("m2: (%T) ", m1)
 	for i, v := range m2 {
 		fmt.Printf("%s:%s ", i, v)
 	}
-
 
 	fmt.Println("\nBefore")
 	fmt.Printf("m3: (%T) %v\n", m3, m3)
